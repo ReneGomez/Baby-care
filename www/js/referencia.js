bf530257443531332750c3d4adf41274cd41a384
Rene <Rene>
2017-01-31 18:43:59 -0600

    'segundo'

---
+++ www/js/referencia.js
@@ -0,0 +1,3 @@
+<script type="text/javascript"> <!-- function showHide(){ //create an object reference to the div containing images var oImageDiv=document.getElementById('myimageDiv') //set display to inline if currently none, otherwise to none oImageDiv.style.display=(oImageDiv.style.display=='none')?'inline':'none' } //--> </script>
+
+
